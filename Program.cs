e1c8f3de3df311bba6e0209f230ef39164a08c3c
Saqib <sakibul.alam001@gmail.com>
2021-12-12 20:04:51 +0600

    Workflow 1 done

--- Program.cs
+++ Program.cs
@@ -1,14 +1,52 @@
 ﻿using System;
+using System.IO;
 
 namespace StudentMgtSystem
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Student st = new Student(22,"Saqib");
-            Console.WriteLine(st.Name);
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Select a level");
+            //string abc = Console.ReadLine();
+            //Level myVar = (Level)Convert.ToInt32(abc);
+            //Console.WriteLine(myVar);
+
+            Student s1 = new Student("S01", "Mohammad", "Saqibul", "Alam", 1, 1);
+            Student s2 = new Student("S02", "Mohammad", "Ajmain", "Alam", 2, 1);
+            Student s3 = new Student("S03", "Kawser", "Ibna", "Raihan", 3, 1);
+
+
+            //Show List of students
+            Console.WriteLine("The students currently enrolled are ");
+            foreach (Student st in Student.allStudents)
+            {
+                Console.WriteLine(st.First + " " + st.Mid + " " + st.Last + " is pursuing " + st.Deg + " in " + st.Dep);
+            }
+
+            Console.WriteLine("\nWhat would you like to do \n1)Add new Student \n2)View Student details \n3)Delete Student");
+            string firstChoice = Console.ReadLine();
+
+
+            //string GuarnteedWritePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            //string FilePath = System.IO.Path.GetDirectoryName(GuarnteedWritePath);
+            //string pathString = @"D:\AsthaIT\StudentManagementSystem\StudentMgtSystem\file.json";
+            //string fileName = "file.json";
+
+            //if (!System.IO.File.Exists(pathString))
+            //{
+            //    using (System.IO.FileStream fs = System.IO.File.Create(pathString))
+            //    {
+
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("File \"{0}\" already exists.", fileName);
+            //    return;
+            //}
+
         }
     }
 }
--- Student.cs
+++ Student.cs
@@ -1,20 +1,28 @@
+
+using Grpc.Core;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+
 
 namespace StudentMgtSystem
 {
     class Student
     {
-        string firstName, middleName, lastName, studentId;
+        string firstName, middleName, lastName, studentId, joiningBatch;
 
-        enum dept { 
-            ComputerScience,
+        int depart, deg;
+
+        public enum dept { 
+            ComputerScience = 1,
             BBA,
             English
         }
         
-        enum degree { 
-            BSC,
+        public enum degree { 
+            BSC = 1,
             BBA,
             BA,
             MSC,
@@ -26,15 +34,123 @@ namespace StudentMgtSystem
 
         List<string> coursesPerSem;
 
-        //public Student(int id,string name) {
-        //    this.studentId = id;
-        //    this.name = name;
-        //}
+        public static List<Student> allStudents = new List<Student>();
 
-        //public string Name
-        //{
-        //    get { return name; }
-        //    set { name = value;  }
-        //}
+        
+        public void addToJson(Student obj)
+        {
+            string pathString = @"D:\AsthaIT\StudentManagementSystem\StudentMgtSystem\students.json";
+            string fileName = "students.json";
+            List<Student> lst = new List<Student>();
+            string read = null;
+            if (System.IO.File.Exists(pathString))
+            { 
+                StreamReader r = new StreamReader(pathString);
+                read = r.ReadToEnd();
+                //Console.WriteLine("There is already a file");
+                //lst = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Student>>(read);
+                r.Close();
+            }
+            else {
+                //Console.WriteLine("Created Json File");
+                FileStream r = File.Create(pathString);
+                r.Close();
+            }
+
+
+            //if (!System.IO.File.Exists(pathString))
+            //{
+
+            StreamWriter fs = new StreamWriter(pathString,true);
+            lst.Add(obj);
+            string myString = JsonConvert.SerializeObject(obj);
+            //string myString = Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+            fs.WriteLine(myString);
+            fs.Close();
+
+
+            //}
+            //else
+            //{
+            //    StreamReader r = new StreamReader("file.json");
+            //    var read = r.ReadToEnd();
+            //    lst = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Student>>(read);
+            //    r.Close();
+            //}
+            //if (lst == null)
+            //{
+            //    List<Student> _data = new List<Student>();
+            //    _data.Add(obj);
+            //    String json = Newtonsoft.Json.JsonConvert.SerializeObject(_data.ToArray());
+            //    /*System.IO.File.WriteAllText("file.json", json);*/
+            //    StreamWriter sw = File.AppendText("file.json");
+            //    sw.WriteLine(json);
+            //}
+            //else
+            //{
+            //    lst.Add(obj);
+            //    String json = Newtonsoft.Json.JsonConvert.SerializeObject(lst);
+            //    //System.IO.File.WriteAllText("file.json", json);
+            //    StreamWriter sw = File.AppendText("file.json");
+            //    sw.WriteLine(json);
+
+            //}
+        }
+
+        public Student()
+        {
+
+        }
+        public Student(string id, string first, string middle, string last,int dep, int degree_)
+        {
+            Student st = new Student();
+            st.studentId = id;
+            st.firstName = first;
+            st.middleName = middle;
+            st.lastName = last;
+            st.depart = dep;
+            st.deg = degree_;
+            addToJson(st);
+            allStudents.Add(st);
+        }
+
+        public string ID
+        {
+            get { return studentId; }
+            set { this.studentId = value; }
+        }
+        public string First
+        {
+            get { return firstName; }
+            set { this.firstName = value; }
+        }
+        public string Mid
+        {
+            get { return middleName; }
+            set { this.middleName = value; }
+        }
+        public string Last
+        {
+            get { return lastName; }
+            set { this.lastName = value; }
+        }
+        public int Dept
+        {
+            get { return depart; }
+            set { this.depart = value; }
+        }
+        public int Degree
+        {
+            get { return deg; }
+            set { this.deg = value; }
+        }
+        public degree Deg
+        {
+            get { return (degree)deg; }
+        }
+        public dept Dep
+        {
+            get { return (dept)depart; }
+        }
     }
 }
